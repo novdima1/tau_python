41067ef5b819bd6dbe548494e80216f890740ac8
novdima-github <bbs.minsk@gmail.com>
2020-09-13 22:19:12 +0300

    Making WebDriver Calls

--- pages/result.py
+++ pages/result.py
@@ -6,21 +6,24 @@ from selenium.webdriver.common.by import By
 
 
 class DuckDuckGoResultPage:
-  RESULT_LINKS = (By.CSS_SELECTOR, 'a.result__a')
-  SEARCH_INPUT = (By.ID, 'search_form_input')
+    # Locators
+    RESULT_LINKS = (By.CSS_SELECTOR, 'a.result__a')
+    SEARCH_INPUT = (By.ID, 'search_form_input')
 
-  def __init__(self, browser):
-    self.browser = browser
+    # Initializer
+    def __init__(self, browser):
+        self.browser = browser
 
-  def result_link_titles(self):
-    # TODO
-    return []
+    # Interaction Methods
+    def result_link_titles(self):
+        links = self.browser.find_elements(*self.RESULT_LINKS)
+        titles = [link.text for link in links]
+        return titles
 
-  def search_input_value(self):
-    # TODO
-    return ""
+    def search_input_value(self):
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        value = search_input.get_attribute('value')
+        return value
 
-  def title(self):
-    # TODO
-    return ""
-
+    def title(self):
+        return self.browser.title
--- pages/search.py
+++ pages/search.py
@@ -3,19 +3,25 @@ This module contains DuckDuckGoSearchPage,
 the page object for the DuckDuckGo search page.
 """
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class DuckDuckGoSearchPage:
 
-  SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
+    # URL
+    URL = 'https://www.duckduckgo.com'
 
-  def __init__(self, browser):
-    self.browser = browser
+    # Locators
+    SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
 
-  def load(self):
-    # TODO
-    pass
+    # Initializer
+    def __init__(self, browser):
+        self.browser = browser
 
-  def search(self, phrase):
-    # TODO
-    pass
+    # Interaction Methods
+    def load(self):
+        self.browser.get(self.URL)
+
+    def search(self, phrase):
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
